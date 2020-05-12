ef91064aa842ef23e3682e1cb2b402dea32fdc31
daniel70125 <dwrighttt504@gmail.com>
2020-05-12 18:05:01 -0500

    Updated 2020

--- classes.js
+++ classes.js
@@ -29,7 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +57,18 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+  hire(emp){
+    this.reports.push(emp);
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +92,34 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports)
+    this.reports = [];
+    this.title = "Not a manager";
+    this.bonus = 0;
+
+  }
+  hire(){
+    super.hire();
+    const reports = this.reports.length;
+    if (reports > 100){
+      this.title = "Bestest Manager";
+    } else if (reports > 50){
+      this.title = "Manager Plus"
+    } else if(reports > 10){
+      this.title = "Manager"
+    } else if(reports > 4){
+      this.title="Mostly Manager"
+    } else if (reports > 0){
+      this.title = "Barely Manager"
+    }
+  }
+  fire(){
+    super.fire();
+    this.bonus += 100;
+  }
+}
 
 
 
@@ -98,6 +146,27 @@
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor(){
+  this.widgets_made_count = 0;
+  this.wear_and_tear_count = 0;
+  this.needs_reboot = false;
+  }
+
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.wear_and_tear_count = Math.floor(this.widgets_made_count / 50);
+  }
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+  reboot(){
+    return function(){
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    }.bind(this);
+  }
+}
+
 
 
--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+let inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner();
 
 
 
@@ -51,7 +51,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend("Jake");
 
 
 
@@ -61,16 +61,21 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  var num = 1;
+  return function() {
+    return num++;
+  };
+}
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// Uncomment this once you make your function
+  var count = makeCounter();
+   count(); // 1
+   count(); // 2
+   count(); // 3
+   count(); // 4
 
 
 
@@ -86,18 +91,27 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
+  value = value;
   return {
-
+    inc: function(){
+      console.log("This is inc: " + value)
+      value = value + 1;
+      return value;
+      
+    },
+    dec: function() {
+      console.log("This is dec: " + value)
+      value = value - 1;
+      return value;
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+ counter.inc() // 11
+ counter.inc() // 12
+ counter.inc() // 13
+ counter.dec() // 12
 
 
 
@@ -112,10 +126,10 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  let message = () =>  `${welcomeText} ${firstname} ${lastname}.`;
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +157,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod(){
+      return privateMethod();
+    }
   };
 })();
 
@@ -162,7 +178,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret(val){
+      secret = secret + val;
+      return secret;
+    },
+    takeAwayFromSecret(val){
+      secret = secret - val;
+      return secret;
+    }
   };
 }
 
@@ -187,7 +210,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
